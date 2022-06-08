fe3a57cd93b67101b55086dddf4ba88a8d1f147e
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2022-06-08 15:58:33 -0300

    Add Shoreline.py and update root.py to User_Interface.py

---
+++ source/main.py
@@ -0,0 +1,8 @@
+from Shoreline import Shoreline
+from User_Interface import ExtratorLinhasCosteiras
+
+
+class main():
+    def main():
+        root = ExtratorLinhasCosteiras()
+        root.construtor_interface()
